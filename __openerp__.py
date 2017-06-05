bdce49a79af3f022062a6890c0d0610971342ef5
morrillo <gustavo.orrillo@gmail.com>
2017-06-05 08:59:17 -0300

    A commit ust in case

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
     'name': 'Blancoamor - Sale2POS',
     'category': 'POS',
     'version': '0.1',
-    'depends': ['base','product','sale','point_of_sale','sale_stock','account'],
+    'depends': ['base','product','sale','point_of_sale','sale_stock','account','l10n_ar_fpoc_pos'],
     'data': [
 	'pdv_view.xml',
     ],
